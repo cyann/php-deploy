51a45a2fa73a9720fb5c20ce83b8214e3ea7af72
Flavien Scheurer <github@cyann.net>
2021-11-09 08:35:53 +0100

    Add deploy.php with strict types

---
+++ deploy.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+	Deploy script for GitHub
+
+	Created by Flavien Scheurer, November 2021
+
+	Settings are stored in deploy.ini.php
+**/
+
+// Enable strict mode.
+declare(strict_types=1);
